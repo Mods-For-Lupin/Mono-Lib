60007d167753a0c11df425657ae67f18800db7cf
jason13official <jason13official@gmail.com>
2025-11-12 12:20:08 -0500

    feat: compat layer

---
+++ common/src/main/java/com/cursee/monolib/api/common/sailing/SailingApi.java
@@ -0,0 +1,8 @@
+package com.cursee.monolib.api.common.sailing;
+
+public class SailingApi {
+
+  public static void register(String modId, String modName, String modVersion, String modPublisher, String modURl) {
+    // TODO impl
+  }
+}
